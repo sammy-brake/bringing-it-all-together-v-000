4a2a09c29df7b01fbbea6a7d90c8839fa7021035
Samantha brake <sammy.brake@gmail.com>
2018-11-10 15:16:04 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -3,10 +3,8 @@ require 'pry'
 class Dog
   attr_accessor :name, :breed, :id
 
-  def initialize(name:, breed:, id: nil)
-    @name = name
-    @breed = breed
-    @id = id
+  def initialize(attributes)
+    attributes.each { |key, value| self.send(("#{key}="), value)}
   end
 
   def self.create_table
@@ -42,7 +40,7 @@ class Dog
   end
 
   def self.create(attributes)
-    dog = Dog.new(attributes.each { |key, value| self.send(("#{key}="), value)})
+    dog = Dog.new(attributes)
     dog.save
     dog
 
