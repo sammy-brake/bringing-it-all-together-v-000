cee3c2a36574b4b2eac3fa7b4d52265283afb4e1
Samantha brake <sammy.brake@gmail.com>
2018-11-10 15:03:42 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -41,8 +41,8 @@ class Dog
   end
   end
 
-  def self.create(attributes)
-    dog = self.new(attributes)
+  def self.create(name:, breed:)
+    dog = self.new(name:, breed:)
     dog.save
     dog
   end
